0ddd87ef43f4dd39dad15f2a574ffa27f3f4a737
Mathias Adikpon <mathiasadikpon@gmail.com>
2026-01-04 09:54:49 -0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import InfoModal from "./components/InfoModal";
 import TipCalculator from "./components/TipCalculator";
 
 export default function App() {
-  return <TipCalculator />;
+  const [isInfoOpen, setIsInfoOpen] = useState(false);
+
+  return (
+    <>
+      <Header onInfoClick={() => setIsInfoOpen(true)} />
+
+      <main>
+        <TipCalculator />
+      </main>
+
+      <Footer />
+
+      <InfoModal isOpen={isInfoOpen} toggle={() => setIsInfoOpen(false)} />
+    </>
+  );
 }
